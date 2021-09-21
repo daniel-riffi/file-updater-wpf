eade98e330c694851372de803410934f8a6f89c7
daniel-riffi <riffert.daniel@gmail.com>
2021-09-21 14:05:50 +0200

    v1

---
+++ file-updater-wpf/MVVM/ViewModels/LofupViewModel.cs
@@ -0,0 +1,90 @@
+﻿using file_updater_wpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace file_updater_wpf.MVVM.ViewModels
+{
+    public class LofupViewModel : ObservableObject
+    {
+        private string fromPath;
+
+        public string FromPath
+        {
+            get { return fromPath; }
+            set { 
+                fromPath = value;
+                RaisePropertyChangedEvent(nameof(FromPath));
+            }
+        }
+
+        private string toPath;
+
+        public string ToPath
+        {
+            get { return toPath; }
+            set {
+                toPath = value;
+                RaisePropertyChangedEvent(nameof(ToPath));
+            }
+        }
+
+        public ICommand SelectFromPath => new RelayCommand<string>(
+            DoSelectFromPath,
+            x => true
+        );
+
+        private void DoSelectFromPath(string obj)
+        {
+            FromPath = ShowFolderBrowserDialog();
+        }
+
+        public ICommand SelectToPath => new RelayCommand<string>(
+            DoSelectToPath,
+            x => true
+        );
+
+        private void DoSelectToPath(string obj)
+        {
+            ToPath = ShowFolderBrowserDialog();
+        }
+
+
+        public ICommand UpdateFiles => new RelayCommand<string>(
+            DoUpdateFiles,
+            x => (FromPath != null && FromPath != "") && (ToPath != null && ToPath != "")
+        );
+
+        private void DoUpdateFiles(string obj)
+        {
+            var program = @"C:\Program Files\lofup\lofup.exe";
+            var command = $"\"{program}\" \"{fromPath}\" \"{toPath}\"";
+
+            Process.Start(command);
+        }
+
+
+        MainWindow mainWindow;
+
+        public LofupViewModel(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+
+
+        private string ShowFolderBrowserDialog()
+        {
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            if (dialog.ShowDialog(mainWindow).GetValueOrDefault())
+            {
+                var path = dialog.SelectedPath;
+                return path;
+            }
+            return null;
+        }
+    }
+}
---
+++ file-updater-wpf/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using file_updater_wpf.MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace file_updater_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LofupViewModel lofupViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FrameworkElement.StyleProperty.OverrideMetadata(typeof(Window), new FrameworkPropertyMetadata
+            {
+                DefaultValue = FindResource(typeof(Window))
+            });
+
+            lofupViewModel = new LofupViewModel(this);
+            DataContext = lofupViewModel;
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
